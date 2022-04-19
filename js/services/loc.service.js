96416c1c7d3980d029e232675b9adff059e909bd
Omri <omriluz1994@gmail.com>
2022-04-19 16:13:18 +0300

    after

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,10 @@
 export const locService = {
-    getLocs
-}
-
+        getLocs
+    }
+    //{id, name, lat, lng, weather, createdAt, updatedAt}
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
 
@@ -15,5 +15,3 @@ function getLocs() {
         }, 2000)
     });
 }
-
-
